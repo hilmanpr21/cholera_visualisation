8b8ebbe27b71874016c4b0b58d89bbb799a9dc51
hilmanpr21 <hilman.prakoso21@gmail.com>
2025-07-09 09:15:07 +0100

    SEIR for single agent

--- sim.js
+++ sim.js
@@ -6,8 +6,10 @@ let agent = {
     x: 10,      // starting x position
     y: 100,     // starting y position 
     radius: 10,  // the agent cirle radius size
-    vx: 0.2,      // velocity in x (pixels per frame)
-    vy: 5,      // velocity in y (pixels per frame)   
+    vx: 1,      // velocity in x (pixels per frame)
+    vy: 4,      // velocity in y (pixels per frame)   
+    state: "susceptible", // initial state of SEIR
+    statetimer: 0, // define how many secons in this current state
 };
 
 
@@ -51,7 +53,7 @@ function drawAgent(agentInput) {
     ctx.closePath();
 }
 
-// update anget position or agent movement
+// update agent position or agent movement
 function updateAgentMovement(agentInput) {
     agentInput.x += agentInput.vx;
     agentInput.y += agentInput.vy;
@@ -65,12 +67,97 @@ function updateAgentMovement(agentInput) {
     }
 }
 
-// Render the canvas in loop
-function animate() {
-    updateAgentMovement(agent); // updates agent's position
-    drawScene(); // redraw the scene that canvas update the agent position
-    requestAnimationFrame(animate); //schedule next frame (re-run animate)
+// Logic of agent SEIR state transition
+function updateAgentState(agentInput, deltaTime) {
+    switch (agentInput.state) {
+        case "exposed":
+            agentInput.statetimer += deltaTime;
+            if (agentInput.statetimer >= 5) {       // checking the condition where the agent have been int he exposed state more than 5 seconds
+                agentInput.state = "infected";      // change the state to infected
+                agentInput.statetimer = 0;          // Set the timer to 0 again
+            }
+            break;
+        case "infected":
+            agentInput.statetimer += deltaTime;
+            if (agentInput.statetimer >= 10) {      // checking the condition where the agent have been in the infected state more than 10 seconds
+                agentInput.state = "recovered";     // change the state to recovered
+                agentInput.statetimer = 0;          // Set the timer to 0 again
+            }
+            break;
+        case "recover":
+            agentInput.statetimer += deltaTime;
+            if (agentInput.statetimer >= 200) {     // checking the condition where the agent have been int he recovered state more than 200 seconds
+                agentInput.state = "susceptible";   // change the state to susceptible
+                agentInput.statetimer = 0;          // Set the timer to 0 again
+            }
+            break;
+    }
 }
+
+// Logic detect contacting contaminated waterbody
+function touchingContaminatedWater(agentInput) {
+    //dx is storing the x-distance between centre of agent and contaminated waterbody
+    const dx = agentInput.x - 600   // agentInput.x is the x-position of agent centre and 600 is the x-position of the contaminated waterbody
+    //dy is storing the y-distance between centre of agent and contaminated wateerbody
+    const dy = agentInput.y - 450   // agentInput.y is the y-position of agent centre and 450 is the y-position of the contaminated waterbody
+
+    // calculate the staighline distance between agent centre and waterbody centre using pythagoras
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    // check if the agent is touching waterbody or not
+    // contaminted water body has radius 40
+    // if the distance between centres is less than the totaal of waterbody's radius and agent's radius, it meens it is touching or even overlap
+    return distance <= 40 + agentInput.radius;
+}
+
+// Trigger exposed stage after suceptible agent tpuch waterbody
+function changeToExposed(agentInput) {
+    if (agentInput.state === "susceptible" && touchingContaminatedWater(agentInput)) {
+        agentInput.state = "exposed";
+        agentInput.statetimer = 0;
+    }
+}
+
+
+// Render the canvas in loop
+let lastTime = performance.now(); // initialise last frame time
+
+function animate(currentTime) {
+    let deltaTime = (currentTime - lastTime) / 1000; // ms to seconds
+    lastTime = currentTime;
+
+    changeToExposed(agent);
+    updateAgentMovement(agent);
+    updateAgentState(agent, deltaTime);
+    drawScene();
+
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+
+function drawAgent(agent) {
+    ctx.beginPath();
+    ctx.arc(agent.x, agent.y, agent.radius, 0, 2 * Math.PI);
+
+    switch (agent.state) {
+        case "susceptible":
+            ctx.fillStyle = "pink";
+            break;
+        case "exposed":
+            ctx.fillStyle = "orange";
+            break;
+        case "infected":
+            ctx.fillStyle = "red";
+            break;
+        case "recovered":
+            ctx.fillStyle = "green";
+            break;
+    }
+
+    ctx.fill();
+    ctx.closePath();
+}
+
 
 // start animation by calling
 animate() 
